396de4d1bb842071f63cbc2e16d34ec607f6710d
TiatiniTin <aah-8@ya.ru>
2022-08-27 14:46:31 +0300

    added Task_6: check if the given  number is even or not

---
+++ Seminar_1/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Число ");
+Console.Write(num);
+
+if (num % 2 == 0) {
+    Console.WriteLine(" - чётное");
+} else {
+     Console.WriteLine(" - нечётное");
+}
