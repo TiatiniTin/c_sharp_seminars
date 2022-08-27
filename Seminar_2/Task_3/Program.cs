a3e823ea090562bb4b92c92925a938529bc92258
TiatiniTin <aah-8@ya.ru>
2022-08-27 19:23:28 +0300

    added Task_3: write if the entered number of a week day refers to a working day or to a day off (using switch construction)

---
+++ Seminar_2/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите порядковый номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 8 && day > 0)
+{
+    switch (day)
+    {
+        case 6: 
+            Console.WriteLine("Выходной день");
+            break;
+
+        case 7: 
+            Console.WriteLine("Выходной день");
+            break;
+        
+        default:
+            Console.WriteLine("Рабочий день");
+            break;
+    }
+}
+else {
+    Console.WriteLine("Порядковый номер введен некорректно!");
+}
