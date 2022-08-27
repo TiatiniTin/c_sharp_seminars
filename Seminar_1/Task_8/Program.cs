621700c0ab73dbe4826ddfacbd10548b3fc67fc9
TiatiniTin <aah-8@ya.ru>
2022-08-27 14:55:25 +0300

    added Task_8: find even numbers from 1 to the entered number

---
+++ Seminar_1/Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Чётные числа от 1 до " + N + ": ");
+
+int currentNumber = 1;
+while (currentNumber < N) {
+    if (currentNumber % 2 == 0) {
+        Console.Write(currentNumber + ", ");
+    }
+    currentNumber++;
+}
+
+
