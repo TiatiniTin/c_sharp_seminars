f5e586bae122141e3759c8d212086b9ed791e59d
TiatiniTin <aah-8@ya.ru>
2022-08-27 18:57:58 +0300

    added Seminer_2 directory and Task_1: write the second digit of a random three-digit number

---
+++ Seminar_2/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task: Напишите программу, которая принимает на вход случайно сгенерированное трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int begin = 100;
+int end = 999;
+int num = new Random().Next(begin, end + 1);
+
+Console.WriteLine("Сгенерированное число: " + num);
+
+int secondDigit = (num % 100) / 10;
+
+Console.WriteLine("Вторая цифра: " + secondDigit);
