b4020ff47e593bdde5f4745e61cc450b82e75394
TiatiniTin <aah-8@ya.ru>
2022-08-27 19:07:59 +0300

    added Task_2: write the third digit or an absence message

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) 
+// или сообщает, что третьей цифры нет.
+
+int begin = 1;
+int end = 65536;
+
+int num = new Random().Next(begin, end + 1);
+
+Console.WriteLine("Сгенерированное число: " + num);
+
+if (num / 100 == 0) 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    int thirdDigit = (num % 1000) / 100;
+    Console.WriteLine("Третья цифра: " + thirdDigit);
+}
