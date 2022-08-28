12f476413e9e4ca21a6671168be191818f9ed64f
TiatiniTin <aah-8@ya.ru>
2022-08-28 16:22:43 +0300

    added Task_3: create an array with elemet cubes from 1 to N

---
+++ Seminar_3/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task: Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. 
+// Полученный массив вывести на экран.
+
+Console.Write("Введите размер массива N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+
+for (int i = 1; i <= N; i++)
+{
+    array[i-1] = i * i * i;
+}
+
+Console.Write("[");
+for (int i = 0; i < N-1; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write(array[N-1] + "]");
