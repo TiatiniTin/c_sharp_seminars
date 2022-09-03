5512fa55e9753ba6aeb4a98bf71e481ac1ed1218
TiatiniTin <aah-8@ya.ru>
2022-09-03 19:59:37 +0300

    added Task_1: A raised to power B

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// Не использовать функцию Math.Pow()
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < B; i++) {
+    result *= A; 
+}
+
+Console.WriteLine($"{A} в степени {B} = {result}");
